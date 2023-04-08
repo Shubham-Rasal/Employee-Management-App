a5e748ecb158bac76db2bae8015c32f18e52db33
Shubham-Rasal <bluequbits@gmail.com>
2023-04-08 06:08:43 +0530

    added web console permissions for the docker container
    this helped in debuging durinng connecting multiple containers
    to the database

--- src/config/environments/development.rb
+++ src/config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Add  web console to the list of allowed IPs
+  config.web_console.permissions = '172.0.0.1/8'
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
